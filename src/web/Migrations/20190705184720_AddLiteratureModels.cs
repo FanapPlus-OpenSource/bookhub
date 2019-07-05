b190b6a44cc01e93a98d8a71d600c27d74039aec
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-07-05 23:19:54 +0430

    Add migrations

---
+++ src/web/Migrations/20190705184720_AddLiteratureModels.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Migrations
+{
+    public partial class AddLiteratureModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Publication",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Code = table.Column<string>(maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publication", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Literature",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(maxLength: 1000, nullable: true),
+                    Author = table.Column<string>(maxLength: 50, nullable: false),
+                    Translator = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
+                    PublicationId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Literature", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Literature_Publication_PublicationId",
+                        column: x => x.PublicationId,
+                        principalTable: "Publication",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LiteratureCategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Code = table.Column<string>(nullable: false),
+                    LiteratureId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LiteratureCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LiteratureCategory_Literature_LiteratureId",
+                        column: x => x.LiteratureId,
+                        principalTable: "Literature",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Literature_PublicationId",
+                table: "Literature",
+                column: "PublicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LiteratureCategory_LiteratureId",
+                table: "LiteratureCategory",
+                column: "LiteratureId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LiteratureCategory");
+
+            migrationBuilder.DropTable(
+                name: "Literature");
+
+            migrationBuilder.DropTable(
+                name: "Publication");
+        }
+    }
+}
