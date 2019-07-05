d84a187908f3b424e8371ceb61d5d51ceed5035f
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-07-05 23:19:48 +0430

    Add models to db context

--- src/web/BookHubDbContext.cs
+++ src/web/BookHubDbContext.cs
@@ -5,8 +5,16 @@ namespace Web
 {
     public class BookHubDbContext : DbContext
     {
+        public DbSet<Literature> Literature { get; set; }
         public BookHubDbContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Literature>().HasKey(k => k.Id);
+            modelBuilder.Entity<LiteratureCategory>().HasKey(k => k.Id);
+            modelBuilder.Entity<Publication>().HasKey(k => k.Id);
         }
     }
 }
