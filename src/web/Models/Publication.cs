a9a0085dbc563e7f74a2adb769b16db100a3d1cb
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-07-05 23:19:20 +0430

    Add Id field

--- src/web/Models/Publication.cs
+++ src/web/Models/Publication.cs
@@ -4,6 +4,7 @@ namespace Web.Models
 {
     public class Publication
     {
+        public int Id { get; set; }
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
